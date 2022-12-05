79cb03f2596893592ccedaccf743f84906a2b1e6
Sebastian Amerio <sebastian.amerio@arba.gov.ar>
2022-12-05 15:32:32 -0300

    TP 2

--- src/routes/tratamiento.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const tratamientoController = require("../controllers/tratamiento.controller");
-
-router.get("/", tratamientoController.all);
-router.put("/:id", tratamientoController.editar);
-router.post("/:id", tratamientoController.crear);
-router.get("/:id", tratamientoController.info);
-
-module.exports = router;
